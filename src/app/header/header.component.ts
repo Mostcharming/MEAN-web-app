d0f145f223214b7bc77aaf8c887e7228c2162896
Mayowa F Fredrick <mostcharming920@yahoo.com>
2022-11-23 11:38:11 +0100

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,21 +5,20 @@ import { AuthService } from "../auth/auth.service";
 
 @Component({
   selector: "app-header",
-  // for the html file
+
   templateUrl: "./header.component.html",
-  // for the css file
+
   styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  // loading this service the default value for userAuthenticatin is false until user is logged in
-  // and issued a token stored in the browser
+
   userIsAuthenticated = false;
   private authListenerSubs: Subscription;
 
-  // injecting the authService into the header component
+
   constructor(private authService: AuthService) {}
 
-  // this is the first service ran after the class is initiated
+
   ngOnInit() {
     this.userIsAuthenticated = this.authService.getIsAuth();
     this.authListenerSubs = this.authService
@@ -29,7 +28,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
       });
   }
 
-  // connecting the logout button to the auth service n making sure the authStatus is false
   onLogout() {
     this.authService.logout();
   }
