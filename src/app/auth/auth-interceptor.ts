80960f94b5c6b850588f893c01134e6c88249f3f
Mayowa F Fredrick <mostcharming920@yahoo.com>
2022-11-23 11:44:45 +0100

    Update auth-interceptor.ts

--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -1,5 +1,4 @@
-// interceptors are functions that will run on any outgoing http requests
-// and we r manipulating that request to attach our token to the header
+
 
 import {
   HttpInterceptor,
@@ -15,8 +14,7 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    // this gets the token from the auth service then gets the authRequest from any http sending the
-    // request and sets or pushes the token into the authorization headers 
+
     const authToken = this.authService.getToken();
     const authRequest = req.clone({
       headers: req.headers.set("Authorization", "Bearer " + authToken)
