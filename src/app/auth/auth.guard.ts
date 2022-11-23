4468064e4acdb06f5cb5b1356bf414bc499d74f9
Mayowa F Fredrick <mostcharming920@yahoo.com>
2022-11-23 11:42:24 +0100

    Update auth.guard.ts

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,5 +1,4 @@
-// this file and its coomponents are for guarding routes, its forces angular to execute this before loading any routes
-// and it also tells anhular what to do with it
+
 
 import {
   CanActivate,
@@ -12,8 +11,7 @@ import { Observable } from "rxjs";
 
 import { AuthService } from "./auth.service";
 
-// using injectable comes hand in hand with constructor
-// used here to inject auth service into this file n it's children
+
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
@@ -23,7 +21,7 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot
   ): boolean | Observable<boolean> | Promise<boolean> {
     
-    // if user isnt authenticated redirect to login
+
     const isAuth = this.authService.getIsAuth();
     if (!isAuth) {
       this.router.navigate(['/login']);
