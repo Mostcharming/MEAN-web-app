7f48d5935010f523a784f929e3c5848e22821c19
Mayowa F Fredrick <mostcharming920@yahoo.com>
2022-11-23 11:31:25 +0100

    Update posts.service.ts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,9 +1,8 @@
-// subscribe means a service wont be called on unless its subsscribed too
-// this file processes all the services done by post
+
 
 import { APP_INITIALIZER, Injectable } from "@angular/core";
 
-// connecting our angular to the backend, so any time a http is called it fetches data from the backend
+
 import { HttpClient } from "@angular/common/http";
 import { Subject, Subscription } from "rxjs";
 import { map } from "rxjs/operators";
@@ -14,32 +13,27 @@ import { Post } from "./post.model";
 
 const BACKEND_URL = environment.apiUrl + "/posts/";
 
-// injecting this service into the root angular core so it can be called easily by any file 
-// that needs it
+
 @Injectable({ providedIn: "root" })
 export class PostsService {
 
-  // adding private means you cant edit it from outside
   private posts: Post[] = [];
-  // subject here is coming from rxjs and its allows values to be accessed by more than one and access
-  // to all values under it
+
   private postsUpdated = new Subject<{ posts: Post[]; postCount: number }>();
 
-  // for connecting to our api and binding to a private property
-  // injecting the angular router in the constructor so it helps us with navigation
-  //meaning it helps us change links other than adding links to the webApp
+
+
   constructor(private http: HttpClient, private router: Router) {}
 
   getPosts(postsPerPage: number, currentPage: number) {
-    // this is connecting the backend paginator to the front end pagonator
+
     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
     this.http
       .get<{ message: string; posts: any; maxPosts: number }>(
         BACKEND_URL + queryParams
       )
 
-      // this is a middleware function to basically map the data coming from the server to our own specified
-      // data structure for example...id=_id
+
       .pipe(
         map(postData => {
           return {
@@ -56,7 +50,7 @@ export class PostsService {
           };
         })
       )
-      // subscribe brings the result of our new post from map and saves under transformedPost
+
       .subscribe(transformedPostData => {
         this.posts = transformedPostData.posts;
         this.postsUpdated.next({
@@ -92,8 +86,7 @@ export class PostsService {
         postData
       )
       .subscribe(responseData => {
-        // after the Subscription is done head back to the root page
-        // Subscription here is adding a new post
+
         this.router.navigate(["/"]);
       });
   }
